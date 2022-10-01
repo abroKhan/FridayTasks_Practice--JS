947d775ccd4362024342ac41c9ad98e9bc00d7c6
abrokhan149@gmail.com <abrokhan149@gmail.com>
2022-10-01 16:01:32 +0500

    Project Files

---
+++ script.js
@@ -0,0 +1,35 @@
+let toggleEl1 = document.querySelector("#toggle1");
+let toggleEl2 = document.querySelector("#toggle2");
+let toggleEl3 = document.querySelector("#toggle3");
+let toggleEl4 = document.querySelector("#toggle4");
+let toggleEl5 = document.querySelector("#toggle5");
+let toggleEl6 = document.querySelector("#toggle6");
+let toggleEl7 = document.querySelector("#toggle7");
+
+toggleEl1.addEventListener("click", function () {
+  toggleEl1.classList.toggle("active");
+});
+
+toggleEl2.addEventListener("click", function () {
+  toggleEl2.classList.toggle("active");
+});
+
+toggleEl3.addEventListener("click", function () {
+  toggleEl3.classList.toggle("active");
+});
+
+toggleEl4.addEventListener("click", function () {
+  toggleEl4.classList.toggle("active");
+});
+
+toggleEl5.addEventListener("click", function () {
+  toggleEl5.classList.toggle("active");
+});
+
+toggleEl6.addEventListener("click", function () {
+  toggleEl6.classList.toggle("active");
+});
+
+toggleEl7.addEventListener("click", function () {
+  toggleEl7.classList.toggle("active");
+});
